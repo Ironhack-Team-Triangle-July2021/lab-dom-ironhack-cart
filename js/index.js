3def048c205691e1053577f88c8be1e620a12e99
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-07-14 13:06:36 +0200

    iterations 2 & 3: calc and display total price

--- js/index.js
+++ js/index.js
@@ -14,18 +14,24 @@ function updateSubtotal(product) {
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
+  // Get list with all products
+  const products = document.getElementsByClassName('product');
+  const productsArr = [...products];
 
-  // ITERATION 3
-  //... your code goes here
+  let totalValue = 0;
+
+  // Iterate & calc subtotal for each product
+  productsArr.forEach( product => {
+    totalValue += updateSubtotal(product);
+  });
+
+  // Update DOM
+  const totalElm = document.querySelector('#total-value span');
+  totalElm.innerText = totalValue;
+  
 }
+
 
 // ITERATION 4
 
